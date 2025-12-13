1d916aa7371bd5a1f021f6937d4c876bb147899d
Jens Nilsson <jenszza@gmail.com>
2025-12-13 01:37:39 +0100

    Update macro
    Combine with full year

--- aoc-2025/src/lib.rs
+++ aoc-2025/src/lib.rs
@@ -11,16 +11,38 @@ pub mod day10;
 pub mod day11;
 pub mod day12;
 
+/// Runs and times each day for the year.
+/// Each day is timed and each of its steps, including preprocessing.
+///
+/// Preprocessing can optionally be provided before each part.
+///
+/// Usage example:
+/// ```
+/// mod dayXX;
+/// mod dayYY;
+/// mod dayXY;
+/// mod dayYX;
+///
+/// aoc!(
+///     day01 => part1;
+///     dayYY => part1, part2;
+///     dayXY => process => part1;
+///     dayYX => process => part1, part2;
+/// );
+/// ```
 #[macro_export]
 macro_rules! aoc {
-    ( $day:ident => $($part:ident),+ ) => {{
-        aoc!(@run $day @parts $( $part, )+);
+    ($($tree:tt)*) => {{
+        let start = std::time::Instant::now();
+        aoc_impl!($($tree)*);
+        let elapsed = start.elapsed();
+        println!("Total elapsed: {:.3?}", elapsed);
     }};
+}
 
-    ( $day:ident => $process:ident => $($part:ident),+ ) => {{
-        aoc!(@run $day @process $process @parts $( $part, )+);
-    }};
-
+#[macro_export]
+#[doc(hidden)]
+macro_rules! run_day {
     ( @run $day:ident $( @process $process:ident )? @parts $($part:ident),+ $(,)?) => {{
         let start_day = std::time::Instant::now();
         let input = include_str!(concat!("../input/", stringify!($day), ".txt"));
@@ -38,11 +60,27 @@ macro_rules! aoc {
             let start = std::time::Instant::now();
             let result = $day::$part(&input);
             let elapsed = start.elapsed();
-
             println!("  {:<15} {:<20}elapsed: {:.3?}", stringify!($part), result, elapsed);
         )+
+
         let elapsed = start_day.elapsed();
         println!("elapsed day: {:.3?}", elapsed);
         println!("");
     }};
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! aoc_impl {
+    ($day:ident => $process:ident => $($part:ident),+ $(,)? ;  $($rest:tt)*) => {{
+        run_day!(@run $day @process $process @parts $($part, )+);
+        aoc_impl!($($rest)*);
+    }};
+
+    ($day:ident => $($part:ident),+ $(,)? ;  $($rest:tt)*) => {{
+        run_day!(@run $day @parts $($part, )+);
+        aoc_impl!($($rest)*);
+    }};
+
+    () => {{}};
 }
--- aoc-2025/src/main.rs
+++ aoc-2025/src/main.rs
@@ -1,23 +1,18 @@
 use aoc_2025::*;
 
-fn run_days() {
-    aoc!(day01 => part1, part2);
-    aoc!(day02 => part1, part2);
-    aoc!(day03 => part1, part2);
-    aoc!(day04 => process => part1, part2);
-    aoc!(day05 => part1, part2);
-    aoc!(day06 => part1, part2);
-    aoc!(day07 => part1, part2);
-    aoc!(day08 => process => part1, part2);
-    aoc!(day09 => process => part1, part2);
-    aoc!(day10 => process => part1, part2);
-    aoc!(day11 => process => part1, part2);
-    aoc!(day12 => process => part1);
-}
-
 fn main() {
-    let start = std::time::Instant::now();
-    run_days();
-    let elapsed = start.elapsed();
-    println!("Total elapsed: {:.3?}", elapsed);
+    aoc!(
+        day01 => part1, part2;
+        day02 => part1, part2;
+        day03 => part1, part2;
+        day04 => process => part1, part2;
+        day05 => part1, part2;
+        day06 => part1, part2;
+        day07 => part1, part2;
+        day08 => process => part1, part2;
+        day09 => process => part1, part2;
+        day10 => process => part1, part2;
+        day11 => process => part1, part2;
+        day12 => process => part1;
+    );
 }
